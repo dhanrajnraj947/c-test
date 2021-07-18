f1c7eedde1cac1e0b9e7aaf9d9d6d6e570c4d4a8
dhanrajnraj947 <87358204+dhanrajnraj947@users.noreply.github.com>
2021-07-18 09:50:43 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -3,7 +3,7 @@ void factorial()
 {
     int n, i;
     unsigned long long fact = 1;
-    printf("\nEnter an integer factorial: ");
+    printf("\nEnter an factorial: ");
     scanf("%d", &n);
 
     // shows error if the user enters a negative integer
